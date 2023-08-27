fb7d8b65ba93f965a9e36299a14191dba42cf0b7
Greg <gergely_horvath@icloud.com>
2023-08-27 20:18:50 +0200

    Add files via upload
    uploaded first script and solutions pdf

---
+++ fpp_ch02.R
@@ -0,0 +1,79 @@
+library(fpp2)
+library(ggplot2)
+
+y <- ts(c(123,39,78,52,110), start=2012)
+y
+
+
+z <- c(123,39,78,52,110)
+y <- ts(z, start=2003, frequency=12)
+y
+
+
+autoplot(melsyd[,"Economy.Class"]) +
+  ggtitle("Economy class passengers: Melbourne-Sydney") +
+  xlab("Year") +
+  ylab("Thousands")
+
+
+autoplot(a10) +
+  ggtitle("Antidiabetic drug sales") +
+  ylab("$ million") +
+  xlab("Year")
+
+
+ggseasonplot(a10, year.labels=TRUE, year.labels.left=TRUE) +
+  ylab("$ million") +
+  ggtitle("Seasonal plot: antidiabetic drug sales")
+
+
+ggseasonplot(a10, polar=TRUE) +
+  ylab("$ million") +
+  ggtitle("Polar seasonal plot: antidiabetic drug sales")
+
+
+ggsubseriesplot(a10) +
+  ylab("$ million") +
+  ggtitle("Seasonal subseries plot: antidiabetic drug sales")
+
+
+
+autoplot(elecdemand[,c("Demand","Temperature")], facets=TRUE) +
+  xlab("Year: 2014") + ylab("") +
+  ggtitle("Half-hourly electricity demand: Victoria, Australia")
+
+
+
+qplot(Temperature, Demand, data=as.data.frame(elecdemand)) +
+  ylab("Demand (GW)") + xlab("Temperature (Celsius)")
+
+
+autoplot(visnights[,1:5], facets=TRUE) +
+  ylab("Number of visitor nights each quarter (millions)")
+
+
+GGally::ggpairs(as.data.frame(visnights[,1:5]))
+
+
+beer2 <- window(ausbeer, start=1992)
+gglagplot(beer2)
+
+
+ggAcf(beer2)
+
+
+aelec <- window(elec, start=1980)
+autoplot(aelec) + xlab("Year") + ylab("GWh")
+
+
+ggAcf(aelec, lag=48)
+
+
+set.seed(30)
+y <- ts(rnorm(50))
+autoplot(y) + ggtitle("White noise")
+
+
+ggAcf(y)
+
+
